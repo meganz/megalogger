a2a9369dc52d7ec9178a947d5cedb9bb7f5ab96d
Guy Kloss <gk@mega.co.nz>
2015-01-30 22:50:28 +1300

    1811: updated README and fixed/amended unit tests

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ megalogger
 ==========
 
 Minimalistic JavaScript logging framework, specifically usable for the browser console
+
+Basic Usage
+-----------
+
+One can simply make a logger using the ``MegaLogger`` constructor, but to take easiest advantage of nested loggers, it is preferred to use ``MegaLogger``'s factory function ``getLogger``.
+
+    // Make logger instance.
+    var worldLogger = MegaLogger.getLogger('world');
+    
+    // Log something on highest level.
+    worldLogger.log('Mega has got a new product!');
+
+Output is:
+
+    2015-0-30 22:35:0.216 - world - LOG  Mega has got a new product!
+
+To create nested loggers, reference the name of the parent logger.
+
+    // Make child logger instance.
+    var newZealandLogger = MegaLogger.getLogger('new_zealand', undefined, 'world');
+    
+    // Log something.
+    newZealandLogger.warn('The new year is in summer.');
+
+Output is:
+
+    2015-0-30 22:35:0.224 - world:new_zealand - WARN  The new year is in summer.
+
+And another level.
+
+    // Make child logger instance.
+    var aucklandLogger = MegaLogger.getLogger('auckland', undefined, 'new_zealand');
+    
+    // Log something.
+    aucklandLogger.info("We will bring the America's Cup home!");
+
+Output is:
+
+    2015-0-30 22:35:0.224 - world:new_zealand:auckland - INFO  We will bring the America's Cup home!
+
--- test/index.js
+++ test/index.js
@@ -2,22 +2,50 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var MegaLogger = require('../lib/megaLogger');
 
+//Create/restore Sinon stub/spy/mock sandboxes.
+var sandbox = null;
+
+beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+});
+
+afterEach(function() {
+    sandbox.restore();
+});
+
+describe('getLogger', function() {
+    var logger = MegaLogger.getLogger("parent");
+
+    it('child logger of parent', function() {
+        var childLogger = MegaLogger.getLogger('child', undefined, 'parent');
+        sandbox.spy(console, 'log');
+        var message = "Hello, little one.";
+        childLogger.log(message);
+        expect(console.log.callCount).to.eql(1);
+        var consoleArgs = console.log.getCall(0).args;
+        expect(consoleArgs[0]).to.match(/parent:child - LOG/);
+        expect(consoleArgs[2]).to.eql(message);
+    });
+});
+
 describe('log', function() {
     var lastError = null;
-    var logger = new MegaLogger("test", {
+    var logger = MegaLogger.getLogger("test", {
         onError: function() {
             lastError = arguments;
         }
     });
 
-    it('can log a message, call callbacks', function() {
-        sinon.spy(console, 'log');
+    it('can log a message', function() {
+        sandbox.spy(console, 'log');
         logger.log("hey!");
         expect(console.log.callCount).to.eql(1);
+    });
 
-        sinon.spy(console, 'error');
+    it('can log a message to call callbacks', function() {
+        sandbox.spy(console, 'error');
         logger.error("hey hey!");
         expect(console.error.callCount).to.eql(1);
-        expect(lastError[2]).to.eql("hey hey!");
+        expect(JSON.parse(lastError[0])[2]).to.eql("hey hey!");
     });
 });
