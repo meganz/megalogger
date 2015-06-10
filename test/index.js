eca64de7bf1c760a73abbb02e67c556c82860559
Lyubomir Petrov <lpetrov@me.com>
2015-06-10 18:32:13 +0300

    added IE + phantomjs check to automatically disable the color formatting of log messages;

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
-var MegaLogger = require('../lib/megaLogger');
 
 //Create/restore Sinon stub/spy/mock sandboxes.
 var sandbox = null;
@@ -14,6 +13,8 @@ afterEach(function() {
 });
 
 describe('getLogger', function() {
+    var MegaLogger = require('../lib/megaLogger');
+
     var logger = MegaLogger.getLogger("parent");
 
     it('child logger of parent', function() {
@@ -36,6 +37,8 @@ describe('getLogger', function() {
 });
 
 describe('log', function() {
+    var MegaLogger = require('../lib/megaLogger');
+
     var lastError = null;
     var logger = MegaLogger.getLogger("test", {
         onError: function() {
@@ -59,6 +62,7 @@ describe('log', function() {
 });
 
 describe('colorsEnabled', function() {
+    var MegaLogger = require('../lib/megaLogger');
 
     it('can log a message with colors', function() {
         var logger = MegaLogger.getLogger("testWithColors", {
@@ -80,4 +84,33 @@ describe('colorsEnabled', function() {
 
         expect(console.error.lastCall.args[0].indexOf("%c")).to.eql(-1);
     });
+});
+
+describe('colorsEnabledDependingOnTheEnvironment', function() {
+    var MegaLogger = require('../lib/megaLogger');
+
+
+    var userAgents = {
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.8 Safari/534.34': false,
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)': false,
+        'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko': false,
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0': false,
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0': true,
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36': true
+    };
+
+    Object.keys(userAgents).forEach(function(k) {
+        var v = userAgents[k];
+        if(typeof(window) === 'undefined') {
+            window = {'fakeWindow': true, 'navigator': {'userAgent': ''}};
+        }
+
+        var testTitle = 'agent: ' + k + ", have support for console formatting: " + v;
+
+        it(testTitle, function() {
+            window.navigator.userAgent = k;
+            expect(MegaLogger._environmentHaveSupportForColors()).to.eql(v, testTitle);
+        });
+    });
+
 });
